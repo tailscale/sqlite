a5e4c084d3e3e9bf1a16de3950b3a19503854af3
David Crawshaw <crawshaw@tailscale.com>
2021-07-09 16:10:09 -0700

    cgosqlite: empty file for CGO_ENABLED=0

--- cgosqlite/cgosqlite.go
+++ cgosqlite/cgosqlite.go
@@ -1,13 +1,3 @@
-// Package cgosqlite is a low-level interface onto SQLite using cgo.
-//
-// This package is designed to have as few opinions as possible.
-// It wraps the SQLite3 C API with functions that are Go-friendly
-// and are designed to make any feature of the underlying C API
-// usable. That means not unduly heap allocating where C wouldn't.
-//
-// Users of this package do not need to use any cgo, which means
-// code using cgosqlite can focus on semantic transform of the API,
-// not C<->Go transforms.
 package cgosqlite
 
 // This list of compiler options is heavily influenced by:
